f7e0672f2bfbd8b4234ed6fa6509e1b5e2431409
jtruhlar <jan.truhlar@thalesgroup.com>
2025-09-23 14:08:03 +0200

    B5h 11h - Operational Data prepared

---
+++ src/main/java/com/joiner/ebus/protherm/B5h11h00hOperationalData.java
@@ -0,0 +1,32 @@
+package com.joiner.ebus.protherm;
+
+/**
+ * eBUS Specification
+ * Application Layer – OSI 7
+ * Vaillant specific extensions V0.6.0
+ * 
+ * 3.6 B5h 11h 00h - Not specified.
+ */
+public class B5h11h00hOperationalData implements OperationalData {
+
+    @Override
+    public byte[] getMasterCrcEndedData() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public int getSlaveSize() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public void setSlaveData(byte[] response) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public byte[] getSlaveData() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+}
