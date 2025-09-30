ce6f4ee750e7ea666046e5e2df4ba6afb596ecd9
jtruhlar <jan.truhlar@thalesgroup.com>
2025-09-30 12:08:40 +0200

    JUnit testSendFrame10h08hB5h11h01h02h

--- communication/src/test/java/com/joiner/ebus/communication/link/EbusMasterSlaveLinkTest.java
+++ communication/src/test/java/com/joiner/ebus/communication/link/EbusMasterSlaveLinkTest.java
@@ -13,6 +13,7 @@ import com.joiner.ebus.EbusMockServerApplication;
 import com.joiner.ebus.communication.protherm.Address10h08hB5h10hData;
 import com.joiner.ebus.communication.protherm.Address10h08hB5h11h01h00hData;
 import com.joiner.ebus.communication.protherm.Address10h08hB5h11h01h01hData;
+import com.joiner.ebus.communication.protherm.Address10h08hB5h11h01h02hData;
 import com.joiner.ebus.communication.protherm.MasterSlaveData;
 
 /**
@@ -76,8 +77,8 @@ class EbusMasterSlaveLinkTest {
         assertArrayEquals(masterCrcEnded, operationalData.getMasterStartData());
         assertArrayEquals(masterCrcEnded, echoMasterFrame);
 
-        // 00 09 4a 46 00 80 ff 5c 00 00 ff 80
-        byte[] slaveData = new byte[]{0x00, 0x09, 0x4a, 0x46, 0x00, (byte) 0x80, (byte) 0xFF, 0x5c, 0x00, 0x00, (byte) 0xFF, (byte) 0x80};
+        // 00 09 4a 46 00 80 ff 5c 00 00 ff B0
+        byte[] slaveData = new byte[]{0x00, 0x09, 0x4a, 0x46, 0x00, (byte) 0x80, (byte) 0xFF, 0x5c, 0x00, 0x00, (byte) 0xFF, (byte) 0xB0};
         assertArrayEquals(slaveData, operationalData.getSlaveData());
 
         // 00 AA
@@ -85,5 +86,24 @@ class EbusMasterSlaveLinkTest {
         assertArrayEquals(finalData, operationalData.getMasterFinalData());
     }
 
+
+    @Test
+    void testSendFrame10h08hB5h11h01h02h() throws Exception {
+        MasterSlaveData operationalData = new Address10h08hB5h11h01h02hData();
+        byte[] echoMasterFrame = dataSender.sendFrame(operationalData);
+
+        // 10 08 B5 11 01 01 89
+        byte[] masterCrcEnded = new byte[]{0x10, 0x08, (byte) 0xB5, 0x11, 0x01, 0x02, (byte) 0x8A};
+        assertArrayEquals(masterCrcEnded, operationalData.getMasterStartData());
+        assertArrayEquals(masterCrcEnded, echoMasterFrame);
+
+        // 00 05 02 14 96 5a 78 0D
+        byte[] slaveData = new byte[]{0x00, 0x05, 0x02, 0x14, (byte) 0x96, 0x5A, 0x78, 0x0D};
+        assertArrayEquals(slaveData, operationalData.getSlaveData());
+
+        // 00 AA
+        byte[] finalData = new byte[]{0x00, (byte) 0xAA};
+        assertArrayEquals(finalData, operationalData.getMasterFinalData());
+    }
     
 }
